9b979dd1432e5c3575cc8877c0f2dc0c970b28cb
Diego Santos <dinatalrn@gmail.com>
2021-12-23 09:32:38 -0300

    Aprimorando o version...

--- src/version.sh
+++ src/version.sh
@@ -15,3 +15,18 @@ source $SCRIPTPATH/lib/echo-color.lib.sh
 FORMATTED_DATE=`date -d @$(cd $SCRIPTPATH && git log -n1 --format="%ct") "+%Y-%m-%d %H:%M:%S"`
 
 echo_color $COLOR_YELLOW "Versão: " && echo "$FORMATTED_DATE"
+
+DATE_CODE=`echo "$FORMATTED_DATE" | sed -E 's/[^0-9]/ /g' | sed 's/^20//'`
+read -a CODE_ALPHA <<< "0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z"
+BUFFER=""
+for IT in $DATE_CODE; do
+	ITWZ=`echo "$IT 0" | awk '{print $1 + $2}'`
+	BUFFER="${BUFFER}."
+	if [[ $ITWZ -ge 36 ]]; then
+		ITWZ=`awk '{print $1 - $2}' <<< "$ITWZ 36"`
+		BUFFER="${BUFFER}z"
+	fi
+	BUFFER="${BUFFER}${CODE_ALPHA[$ITWZ]}"
+done
+
+echo_color $COLOR_YELLOW "Código: " && ( echo $BUFFER | sed 's/^.//' )
