fdc5352e48f31f7b264e97172a821b40c0542e9b
Diego Santos <dinatalrn@gmail.com>
2021-12-25 00:36:41 -0300

    Melhorando autocomplete...

--- lib/autocomplete.sh
+++ lib/autocomplete.sh
@@ -4,15 +4,20 @@ if [[ ! -z "$HUNTER_HOME" ]]; then
 
   _script()
   {
-    _script_commands=`ls -p $HUNTER_HOME/src/ | grep -v "/" | sed 's/.sh$//'`
 
+    _script_commands=""
     local cur
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ "${#COMP_WORDS[@]}" = "2" ]; then
+      _script_commands=`ls -p $HUNTER_HOME/src/ | grep -v "/" | sed 's/.sh$//'`
+    fi
+
     COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
 
     return 0
   }
-  complete -o nospace -F _script hunter
+  complete -o nospace -o default -F _script hunter
   
 fi
